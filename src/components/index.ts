c51d832d6b0072724aa7bbdb1ec9df1c7921fdda
HLena <gordillo.vi20@gmal.com>
2025-08-25 16:48:06 -0500

    chore: update component index file exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,6 @@ export { default as Navbar } from "./ui/navbar/Navbar";
 export { default as NabvarMenu } from "./ui/navbar/NavbarMenu";
 export { default as Navigation } from "./ui/navbar/Navigation";
 export { default as SearchBar } from "./ui/navbar/SearchBar";
-export { default as MenuToggle } from "./ui/navbar/MenuToggle";
 
 export { default as FilterPanel } from "./ui/filter/FilterPanel";
 export { default as FilterSection } from "./ui/filter/FilterSection";
@@ -10,11 +9,13 @@ export { default as ColorSelector } from "./ui/filter/ColorSelector";
 export { default as SizesSelector } from "./ui/filter/SizesSelector";
 export { default as Title } from "./ui/title/Title";
 export { default as CategoryNavigation } from "./ui/CategoryNavigation";
+export { default as Sidebar } from "./ui/sidebar/Sidebar";
 
 export { default as CardProduct } from "./product/CardProduct";
 export { default as ImageSlider } from "./product/ImageSlider";
 export { default as QuantitySelector } from "./product/QuantitySelector";
 export { default as AddToCartButton } from "./product/AddToCartButton";
+export { default as CategoryInfo } from "./product/CategoryInfo";
 export { default as Subtitle } from "./cart/Subtitle";
 export { default as SummaryOrder } from "./cart/SummaryOrder";
 
